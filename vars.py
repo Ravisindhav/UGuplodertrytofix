a4999ecd361ee8d5c8cb272367ded1f53a01d8ae
Ravisindhav <sindhavravi11@gmail.com>
2026-01-31 05:27:40 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -17,7 +17,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "77"))
 ADMINS = [int(x) for x in os.environ.get("ADMINS", "774").split()]  # Default to owner ID
 
 # Channel Configuration
-PREMIUM_CHANNEL = "https://t.me/+W-Q51EuLf2QwYTl"
+PREMIUM_CHANNEL = "https://t.me/myherokudata"
 # Thumbnail Configuration
 THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "https://i.fbcd.co/products/original/ug-logo-designs-2-acbfbf7b80e16df4c902a34d1caf148e7e1feca736e21075114990e62294f3ac.jpg").split()))
 
@@ -59,3 +59,4 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
